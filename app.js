b1b3b3f35c2b8dc1af20285b518c5d08a93fd762
floydapungan <42062501+floydapungan@users.noreply.github.com>
2018-08-08 19:38:18 +0800

    create route for creating output using port

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const mysql = require('mysql');
 const express = require('express');
 var app = express();
+
+
+
+
+app.listen(3000, () =>
+ console.log('Express server is running at port no : 3000')
+);
